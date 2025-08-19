6ccbcc1facc35ed191409ddb59dc60751e8c4cce
ShixiaoFei33 <3369909027@qq.com>
2025-08-19 23:37:11 +0800

    main

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 local Data = {}
-local id_ui = {
+local id_ui = {     -- 名字序列
     "7540194344203696498-149492_12",
     "7540194344203696498-149492_17",
     "7540194344203696498-149492_22",
@@ -12,7 +12,7 @@ local id_ui = {
     "7540194344203696498-149492_57"
 }
 
-local beat_ui = {
+local beat_ui = {       -- 排序值序列
     "7540194344203696498-149492_14",
     "7540194344203696498-149492_19",
     "7540194344203696498-149492_24",
@@ -25,7 +25,7 @@ local beat_ui = {
     "7540194344203696498-149492_59"
 }
 
-local mini_ui = {
+local mini_ui = {       -- 迷你号序列
     "7540194344203696498-149492_13",
     "7540194344203696498-149492_18",
     "7540194344203696498-149492_23",
@@ -39,7 +39,7 @@ local mini_ui = {
     "7540194344203696498-149492_58"
 }
 
-local rand_ui = {
+local rand_ui = {       -- 名次序列
     "7540194344203696498-149492_13",
     "7540194344203696498-149492_18",
     "7540194344203696498-149492_23",
@@ -53,33 +53,33 @@ local rand_ui = {
     "7540194344203696498-149492_58"
 }
 
-local my_ui = {
+local my_ui = {     -- 自己四个数据序列
     "7540194344203696498-149492_61",     --自己排名
     "7540194344203696498-149492_62",         --自己名字
     "7540194344203696498-149492_63",         --自己迷你号
     "7540194344203696498-149492_64",         --自己击杀数
 }
 
-local page_ui = {
+local page_ui = {       -- 页相关
     "7540194344203696498-149492_70", --页码
     "7540194344203696498-149492_65", --左翻
     "7540194344203696498-149492_66", --右翻
 }
 
-local page = 1
-local pagesize = 5
+local page = 1      -- 当前页码
+local pagesize = 5      -- 每页显示的条数
 
 
 --自定义事件监听
-local function func_event(param)
+local function func_event(param)        -- 从server那边搞过来的数据，自定义事件传递
     local ret, data = pcall(JSON.decode,JSON,param.customdata)
     Data = data or {}
 end
 
-local function My_info(eventobjid)
+local function My_info(eventobjid)      -- 自己数据的处理
     local found = false
     for k, v in ipairs(Data) do
-        if v.k == tostring(eventobjid) then       -- 改v.k
+        if v.k == tostring(eventobjid) then       -- 改v.k，前100
             found = true
             Customui:setText(eventobjid, "7540194344203696498-149492", my_ui[1], tostring(k))
             Customui:setText(eventobjid, "7540194344203696498-149492", my_ui[2], v.nick or "")
@@ -88,7 +88,7 @@ local function My_info(eventobjid)
             break
         end
     end
-    if not found then
+    if not found then       -- 100开外
         Customui:setText(eventobjid, "7540194344203696498-149492", my_ui[1], "99+")
         local result,name=Player:getNickname(eventobjid)
         Customui:setText(eventobjid, "7540194344203696498-149492", my_ui[2], name)
@@ -98,7 +98,7 @@ local function My_info(eventobjid)
     end
 end
 
-local function Render(eventobjid)
+local function Render(eventobjid)       -- 渲染主函数
     local startIndex = (page - 1) * pagesize + 1
     local endIndex = math.min(page * pagesize, #Data)
     Customui:setText(eventobjid, "7540194344203696498-149492", page_ui[1], page)      -- 页码
@@ -128,7 +128,7 @@ local function Render(eventobjid)
     My_info(eventobjid) -- 渲染自己的信息
 end
 
-local function LeftPage(e)
+local function LeftPage(e)      -- 左翻
     if e.uielement == page_ui[2] then
         if page > 1 then
             page = page - 1
@@ -139,7 +139,7 @@ local function LeftPage(e)
     end
 end
 
-local function RightPage(e)
+local function RightPage(e)     -- 右翻
     if e.uielement == page_ui[3] then
         if page < math.ceil(#Data / pagesize) then
             page = page + 1
@@ -150,10 +150,11 @@ local function RightPage(e)
     end
 end
 
-local function OnOpenUI(e)
+local function OnOpenUI(e)      -- 每次打开页面都渲染，打开逻辑需要自己加，或者触发器里拼
     --打开界面渲染
     Render(e.eventobjid)
 end
+
 ScriptSupportEvent:registerEvent('GetServerData', func_event)
 ScriptSupportEvent:registerEvent('UI.Button.Click', LeftPage)
 ScriptSupportEvent:registerEvent('UI.Button.Click', RightPage)
--- server.lua
+++ server.lua
@@ -1,14 +1,14 @@
 local Player_data = {}
 local CloudData = {}
 
-local function Player_Beat_info()
+local function Player_Beat_info()       -- 玩家自己的数据
     for k, _ in pairs(Player_data) do
         local result, value = VarLib2:getPlayerVarByName(k, 3, "击杀数")        -- 获取要排行的玩家个人数据
         Player_data[k] = value      -- 添加到全局以便上传
     end
 end
 
-local function func_event(param)
+local function func_event(param)        -- 编码完事后通过迷你的自定义事件把数据传给client
     local ok, json = pcall(JSON.encode, JSON, param)
     Game:dispatchEvent("GetServerData", { customdata = json })
 end
@@ -28,7 +28,7 @@ end
 
 local function PushServer(e)
     local current = e.second
-    if (current ~= nil and current >= 10 and (current - 10) % 10 == 0) then     -- 10、20、30
+    if (current ~= nil and current >= 10 and (current - 10) % 10 == 0) then     -- 10、20、30，强烈推荐这三个值
         Player_Beat_info()
         for k, v in pairs(Player_data) do
             local ret = CloudSever:setOrderDataBykey("rank_1755592011", k, v)       -- 上传
@@ -37,15 +37,18 @@ local function PushServer(e)
     end
 end
 
+--初始化服务，让排行榜有数据，不然是空的
 local function InitServer(e)
     local result, value = VarLib2:getPlayerVarByName(e.eventobjid, 3, "击杀数")
     Player_data[e.eventobjid] = value
     PullServer()
 end
 
-local function Player_Close(e)
+local function Player_Close(e)      -- 玩家退出后清数据，防止数据重复上传导致请求上限
     Player_data[e.eventobjid] = nil
 end
+
+-- 仨事件都能看得懂，定时上传、初始化、清理
 ScriptSupportEvent:registerEvent("Game.RunTime", PushServer)
 ScriptSupportEvent:registerEvent("Game.AnyPlayer.EnterGame", InitServer)
 ScriptSupportEvent:registerEvent("Game.AnyPlayer.LeaveGame", Player_Close)
